24876de6437508e12b46c0186bd7845ad3590cb9
Sergey Romanov <xxsmotur@gmail.com>
2015-07-15 00:11:58 +0500

    Add new tests

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var rka = require('./lib/rka');
 
-var str = "abcdefgh";
-describe('Test substring contains string', function() {
+var str = "abcde";
+describe('Test substring contains in the string', function() {
     it('should return index from starts of substring1', function() {
        assert.equal(rka(str, "abcd"), 0); 
     });
@@ -11,12 +11,54 @@ describe('Test substring contains string', function() {
         assert.equal(rka(str, "ab"), 0); 
     });
 
-    it('should return index from starts of substring3', function(){
-        assert.equal(rka(str, "ac"), 0); 
+    it('should return index from starts position of substring3', function(){
+        assert.equal(rka(str, "abc"), 0); 
     });
 
-    it('should return index from second of substring2', function(){
+    it('should return index from 1th position of substring2', function(){
         assert.equal(rka(str, "bcd"), 1); 
+    });
+
+    it('should return index from 1th position of substring2', function(){
+        assert.equal(rka(str, "b"), 1); 
+    });
+
+    it('should return index from 2th position of substring2', function(){
+        assert.equal(rka(str, "cd"), 2); 
+    });
+
+    it('should return index from 2th position of substring2', function(){
+        assert.equal(rka(str, "cde"), 2); 
+    });
+
+    it('should return index from 3th position of substring2', function(){
+        assert.equal(rka(str, "de"), 3); 
+    });
+
+    it('should return index from 3th position of substring2', function(){
+        assert.equal(rka(str, "d"), 3); 
+    });
+
+    it('should return index from last position of substring2', function(){
+        assert.equal(rka(str, "e"), 4); 
+    });
+});
+
+describe('Test substring not cointains in the string', function(){
+    it('should return -1', function(){
+        assert.equal(rka(str, "abe"), -1);
+    });
+
+    it('should return -1', function(){
+        assert.equal(rka(str, "gfdgdfgdfgd"), -1);
+    });
+
+    it('should return -1', function(){
+        assert.equal(rka(str, "efg"), -1);
+    });
+
+    it('should return -1', function(){
+        assert.equal(rka(str, "aaa"), -1);
     });
 
 });
